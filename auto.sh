6556ad8732ed003a62f8a141f5d2868aba0a7252
ArcticFoox <jongsin16@naver.com>
2024-01-12 17:31:39 +0900

    finish sh

--- auto.sh
+++ auto.sh
@@ -4,7 +4,7 @@ filePath=`git status -u -s | head -n 1`
 
 echo $filePath
 
-commitMag=`cat $filePath | head -1`
+commitMag=`cat ${filePath:3} | head -1`
 
 echo ${commitMag:2} | cut -d ' ' -f1
 echo ${commitMag:2} | cut -d ' ' -f2
