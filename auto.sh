3194e4322acd7d07bb19fb14bed45cb8824cd81f
ArcticFoox <jongsin16@naver.com>
2024-01-15 15:13:09 +0900

    final shell

--- auto.sh
+++ auto.sh
@@ -11,7 +11,7 @@ echo $commitMag
 problemId=`echo ${commitMag:2} | cut -d ' ' -f1`
 problemKind=`echo ${commitMag:2} | cut -d ' ' -f2`
 problemURL=`echo ${commitMag:2} | cut -d ' ' -f3`
-problemCommit=`echo -e $problemKind\n$problemURL`
+problemCommit=`echo -e $problemKind'\n'$problemURL`
 
 echo $problemId
 echo $problemKind
@@ -21,3 +21,5 @@ echo $problemCommit
 git add ${filePath:2}
 git commit -m "$problemId" -m "$problemCommit"
 git push
+
+echo "end"
