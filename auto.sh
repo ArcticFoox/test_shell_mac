6bf2b9b40458d57d657118513d62199f3b041858
ArcticFoox <jongsin16@naver.com>
2024-01-15 14:48:33 +0900

    update shell

--- auto.sh
+++ auto.sh
@@ -2,10 +2,17 @@ echo 'start'
 
 filePath=`git status -u -s | head -n 1`
 
-echo $filePath
+echo ${filePath:2}
 
 commitMag=`cat ${filePath:3} | head -1`
 
-echo ${commitMag:2} | cut -d ' ' -f1
-echo ${commitMag:2} | cut -d ' ' -f2
-echo ${commitMag:2} | cut -d ' ' -f3
+problemId=`${commitMag:2} | cut -d ' ' -f1`
+problemKind=`${commitMag:2} | cut -d ' ' -f2`
+problemURL=`${commitMag:2} | cut -d ' ' -f3`
+
+echo $problemId
+echo $problemKind
+echo $problemURL
+
+# git add ${filePath:3}
+# git commit 
